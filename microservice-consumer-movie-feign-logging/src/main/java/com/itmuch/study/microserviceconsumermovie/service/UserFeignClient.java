2b783010309433d327d425566963ca07fe52ec6f
zhengzhendu <zdzhengzhendu@163.com>
2020-01-30 20:58:12 +0800

    打印feign debug日志

---
+++ microservice-consumer-movie-feign-logging/src/main/java/com/itmuch/study/microserviceconsumermovie/service/UserFeignClient.java
@@ -0,0 +1,15 @@
+package com.itmuch.study.microserviceconsumermovie.service;
+
+import com.itmuch.study.microserviceconsumermovie.config.FeignLogConfiguration;
+import com.itmuch.study.microserviceconsumermovie.entity.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "microservice-provider-user",configuration = FeignLogConfiguration.class)
+public interface UserFeignClient {
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    User findById(@PathVariable Long id);
+}
