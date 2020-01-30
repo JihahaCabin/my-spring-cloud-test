ea5942f925cf4bb1c11f9b40f54df80341e7f4ff
zhengzhendu <zdzhengzhendu@163.com>
2020-01-30 15:32:20 +0800

    配置高可用eureka

---
+++ microservice-discovery-eureka-ha/src/test/java/com/cloud/study/demo/MicroserviceDiscoveryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.study.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDiscoveryEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
